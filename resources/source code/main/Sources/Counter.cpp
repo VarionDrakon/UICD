ccc39936f6f2b9009d8034caf30d46fb4cb259a6
VarionDrakon <variondrakon@gmail.com>
2026-03-05 11:08:32 +0300

    Fix counter bugs...

--- resources/source code/main/Sources/Counter.cpp
+++ resources/source code/main/Sources/Counter.cpp
@@ -7,18 +7,18 @@ void sensorsInitialize() {
   pinMode(SENSOR_FIRST_PINOUT, INPUT_PULLUP);
   pinMode(SENSOR_SECOND_PINOUT, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(SENSOR_FIRST_PINOUT), counterSensorFirst, RISING);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_SECOND_PINOUT), counterSensorSecondary, RISING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_FIRST_PINOUT), counterSensorFirst, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_SECOND_PINOUT), counterSensorSecondary, FALLING);
 }
 
 void counterSensorFirst() {
-  if (digitalRead(SENSOR_SECOND_PINOUT) == HIGH) {
+  if (digitalRead(SENSOR_SECOND_PINOUT) == LOW) {
     counterSensorHandleForward();
   }
 }
 
 void counterSensorSecondary() {
-  if (digitalRead(SENSOR_FIRST_PINOUT) == HIGH) {
+  if (digitalRead(SENSOR_FIRST_PINOUT) == LOW) {
     counterSensorHandleBackward();
   }
 }
